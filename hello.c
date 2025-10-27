61e9d220479ea2b315be20bc51fa98856f079150
arcticbear <arcticbear@localhost.localdomain>
2025-10-26 21:06:06 -0400

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: John Llano
+ * Date: 2025/10/26
+ *
+ * A simple C program that display my name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("John Llano\n");
+
+    return 0;
+}
+
